6e5f6b3962e18d8d6fa6bc7506dafe95550c3e08
Marcin Jalbrzykowski <jalbrzym@len.iem.pw.edu.pl>
2013-11-18 19:48:46 +0100

    Wykonane zadanie

--- vim-challenge/integral.c
+++ vim-challenge/integral.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
+#include <math.h>
 
-float integral(float a, float b, float (*f) (float), int n) {
- int i;
-	float dx = (b - a) / n;
-    float value_of_integral = 0.0;
-
-for (i=0; i<n; i++) {
-float si_a = a + (i)*dx;
-float si_b = a + (i+1)*dx;
-value_of_integral += simpson(si_a, si_b, f, dx);
-}
-
-return value_of_integral;
-}
-
-float square(float x) {
-	return x*x;
-}
-
-float simpson(float a, float b, float (*f) (float), float dx) {
-	float value_of_interval_integral =
+double simpson(double a, double b, double (*f) (double), double dx) {
+	double value_of_interval_integral =
 					0.5 * (f(a) + f(b)) / dx;
 	return value_of_interval_integral;
 }
 
+double integral(double a, double b, double (*f) (double), int n) {
+	int i;
+	double dx = (b - a) / n;
+	double value_of_integral = 0.0;
+
+	for (i=0; i<n; i++) {
+		double subinterval_a = a + (i)*dx;
+		double subinterval_b = a + (i+1)*dx;
+		value_of_integral += subintervalmpson(si_a, si_b, f, dx);
+	}
+
+	return value_of_integral;
+}
+
+double square(double x) {
+	return x*x;
+}
+
+double cube(double x) {
+	return x*x*x;
+}
+
 int main(int argc, char** argv) {
-	float twopi = 2 * 3.14;
-	float integral_of_f = integral(0.0, 1.0, square, 1e3);
-	/*float integral_of_sin = integral(0.0, twopi, sin, 10);*/
-	printf("Całka z funkjci f (0.0 - 1.0) = %g\n", integral_of_f);
+	double twopi = 2 * 3.14;
+	double integral_of_f = integral(0.0, 1.0, cube, 1e3);
+	/*double integral_of_sin = integral(0.0, twopi, sin, 10);*/
+	printf("Całka z funkcji f (0.0 - 1.0) = %g\n", integral_of_f);
 	/*printf("Całka z sinus (0 - 2pi) = %g\n", integral_of_sin);*/
 	return 0;
 }
