43bbdeaf93eb4bfb56970c276d1519451cafc998
Bartek Chaber <chaber@iem.pw.edu.pl>
2013-11-18 14:05:01 +0100

    Dodaj zadanie z vim

---
+++ vim-challenge/integral.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+float integral(float a, float b, float (*f) (float), int n) {
+ int i;
+	float dx = (b - a) / n;
+    float value_of_integral = 0.0;
+
+for (i=0; i<n; i++) {
+float si_a = a + (i)*dx;
+float si_b = a + (i+1)*dx;
+value_of_integral += simpson(si_a, si_b, f, dx);
+}
+
+return value_of_integral;
+}
+
+float square(float x) {
+	return x*x;
+}
+
+float simpson(float a, float b, float (*f) (float), float dx) {
+	float value_of_interval_integral =
+					0.5 * (f(a) + f(b)) / dx;
+	return value_of_interval_integral;
+}
+
+int main(int argc, char** argv) {
+	float twopi = 2 * 3.14;
+	float integral_of_f = integral(0.0, 1.0, square, 1e3);
+	/*float integral_of_sin = integral(0.0, twopi, sin, 10);*/
+	printf("Całka z funkjci f (0.0 - 1.0) = %g\n", integral_of_f);
+	/*printf("Całka z sinus (0 - 2pi) = %g\n", integral_of_sin);*/
+	return 0;
+}
